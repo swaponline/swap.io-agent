6f750d22766f6e5875b66429b2897f2a85fdca6d
Gercog52 <54477063+Gercog52@users.noreply.github.com>
2021-06-29 01:23:18 +0800

    blockchainindexer struct

--- src/blockchainHandlers/transactionIndexer/transactionIndexer.go
+++ src/blockchainHandlers/transactionIndexer/transactionIndexer.go
@@ -1,10 +1,29 @@
 package transactionIndexer
 
+import (
+	"github.com/syndtr/goleveldb/leveldb"
+	"log"
+)
+
 type TransactionIndexer struct {}
 type Options struct {
 	DbPath string
+	FirstBlock string
 }
 
-func New() {
+func New(options Options) {
+	db, err := leveldb.OpenFile(options.DbPath, nil)
+	if err != nil {
+		log.Panicf("db not open %v. err - %v", options.DbPath, err)
+	}
+	lastBlock, err := db.Get(lastBlockKey, nil)
+	//last block not set
+	if err != nil {
+		lastBlock = []byte(options.FirstBlock)
+		db.Put(lastBlockKey, lastBlock, nil)
+	}
 
+	log.Println(lastBlock)
 }
+
+var lastBlockKey = []byte("lastBlock")
